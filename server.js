211d821df8d03e9ad4cc8b019e0e64fdb509f347
Mahad <itzmahad01@gmail.com>
2026-07-05 13:02:49 +0500

    Contact Add

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import {app} from "./src/app.js"
 import { connectDb } from "./src/config/db.js"
 import dotenv from "dotenv"
 import dns from "dns";
-import { log } from "console";
 
 dns.setServers([
   "1.1.1.1",
@@ -11,14 +10,12 @@ dns.setServers([
 
 dotenv.config()
 
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 
 
 
 await connectDb()
 
-app.listen(PORT, () => {
-  // console.log(`http://localhost:${PORT}`);
-}); 
+
 
 export default app
